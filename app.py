01766de485756a08be720ed4f0a0192bc3859baa
Tekkotsu <69491893+tekkotsu999@users.noreply.github.com>
2023-09-17 13:02:15 +0900

    ２秒ディレイを削除

--- app.py
+++ app.py
@@ -13,14 +13,14 @@ def home():
 @app.route('/get_method', methods=['GET'])
 def get_method():
     # 処理時間を計測（シミュレーションのために2秒待機）
-    time.sleep(2)
+    # time.sleep(2)
     return "This is a GET request."
 
 # POSTリクエスト用のエンドポイント
 @app.route('/post_method', methods=['POST'])
 def post_method():
     # 処理時間を計測（シミュレーションのために2秒待機）
-    time.sleep(2)
+    # time.sleep(2)
     return "This is a POST request."
 
 if __name__ == '__main__':
